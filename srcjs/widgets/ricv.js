59b47d0f8b7216180109008a4d6bba77e9449147
Xavier Adam <xa@pop-os.localdomain>
2022-03-17 21:50:57 +0100

    First working widget (no options)

--- srcjs/widgets/ricv.js
+++ srcjs/widgets/ricv.js
@@ -1,31 +1,34 @@
-import 'widgets';
-import { asHeader } from '../modules/header.js'; 
+import "widgets";
+import ImageCompare from "image-compare-viewer";
 
 HTMLWidgets.widget({
+  name: "ricv",
 
-  name: 'ricv',
+  type: "output",
 
-  type: 'output',
-
-  factory: function(el, width, height) {
-
-    // TODO: define shared variables for this instance
-
+  factory: function (el, width, height) {
     return {
+      renderValue: function (x) {
+        console.log(x);
 
-      renderValue: function(x) {
+        const imgViewer = document.createElement("div");
+        imgViewer.id = x.viewerId;
 
-        // TODO: code to render the widget, e.g.
-        el.innerHTML = asHeader(x);
+        const img1 = document.createElement("img");
+        img1.src = x.img1;
+        imgViewer.appendChild(img1);
 
+        const img2 = document.createElement("img");
+        img2.src = x.img2;
+        imgViewer.appendChild(img2);
+
+        const element = document.getElementById(x.widgetId);
+        element.appendChild(imgViewer);
+
+        const viewer = new ImageCompare(imgViewer).mount();
       },
 
-      resize: function(width, height) {
-
-        // TODO: code to re-render the widget with a new size
-
-      }
-
+      resize: function (width, height) {},
     };
-  }
+  },
 });
