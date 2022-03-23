d3a22a1967b5b05f5b69bed6b3b46ae3e4b67811
Xavier Adam <xa@pop-os.localdomain>
2022-03-23 21:13:48 +0100

    Add helper ricv_opts to create options list

--- R/options.R
+++ R/options.R
@@ -87,12 +87,48 @@ new_Options <- function(x = list()) {
   structure(x, class = "Options")
 }
 
-opts <- function(
-
+#' Create ricv options
+#'
+#' This function helps you create the list of options that ricv can accept. None of them are mandatory.
+#' One can also create the list by hand, this function is just meant to help you remember the possible
+#' attributes.
+#'
+#' @param labelOptionsBefore A character string for the 'before' label.
+#' @param labelOptionsAfter A character string for the 'after' label.
+#' @param labelOptionsOnHover A logical. Whether or not to show the labels only on hover.
+#' @param controlColor A character string representing a hex color (e.g. "#FFFFFF") for the control line/arrows.
+#' @param controlShadow A logical. Whether or not to add a shadow for the control line/arrows.
+#' @param addCircle A logical. Whether or not to add a circle around the control line/arrows.
+#' @param addCircleBlur A logical. Whether or not to add a blur in the circle around the control line/arrows.
+#' @param showLabels A logical. Whether or not to show the 'before'/'after' labels (see also the other labelOptions... arguments).
+#' @param smoothing A logical. Whether or not to smooth (i.e. dampen) the movement of the control line/arrows.
+#' @param smoothingAmount A numeric value. Set the amount of smoothing.
+#' @param hoverStart A logical. Whether or not to activate click-less control.
+#' @param verticalMode A logical. Whether or not to set the control to vertical mode.
+#' @param startingPoint A numeric value. Represent the percentage where the line start (e.g. '50' means the middle 50%).
+#' @param fluidMode A logical. Fluid mode enables the container to have fluid height and width, independent of each other, useful for using Image Compare Viewer in a full screen container for instance. The image is dynamically cropped using the CSS background 'cover' property.
+#'
+#' @return a list of options that can be used for the `options` argument of \code{\link{ricv}}
+#' @export
+ricv_opts <- function(
+  labelOptionsBefore = "Before",
+  labelOptionsAfter = "After",
+  labelOptionsOnHover = TRUE,
+  controlColor = "#FFFFFF",
+  controlShadow = TRUE,
+  addCircle = TRUE,
+  addCircleBlur = TRUE,
+  showLabels = TRUE,
+  smoothing = TRUE,
+  smoothingAmount = 200,
+  hoverStart = FALSE,
+  verticalMode = FALSE,
+  startingPoint = 50,
+  fluidMode = FALSE
 ) {
-  assert_character_unit_set(label_options$before, "before")
-  assert_character_unit_set(labelOptions$after, "after")
-  assert_logical_unit_set(labelOptions$onHover, "onHover")
+  assert_character_unit_set(labelOptionsBefore, "labelOptionsBefore")
+  assert_character_unit_set(labelOptionsAfter, "labelOptionsAfter")
+  assert_logical_unit_set(labelOptionsOnHover, "labelOptionsOnHover")
   assert_character_unit_set(controlColor, "controlColor")
   assert_logical_unit_set(controlShadow, "controlShadow")
   assert_logical_unit_set(addCircle, "addCircle")
@@ -104,4 +140,26 @@ opts <- function(
   assert_logical_unit_set(verticalMode, "verticalMode")
   assert_numeric_unit_set(startingPoint, "startingPoint")
   assert_logical_unit_set(fluidMode, "fluidMode")
+
+  options <- list()
+  options$controlColor <- controlColor
+  options$controlShadow <- controlShadow
+  options$addCircle <- addCircle
+  options$addCircleBlur <- addCircleBlur
+  options$showLabels <- showLabels
+  options$smoothing <- smoothing
+  options$smoothingAmount <- smoothingAmount
+  options$hoverStart <- hoverStart
+  options$verticalMode <- verticalMode
+  options$startingPoint <- startingPoint
+  options$fluidMode <- fluidMode
+
+  if (!is.null(labelOptionsBefore) || !is.null(labelOptionsAfter) || !is.null(labelOptionsOnHover)) {
+    options$labelOptions <- list()
+    options$labelOptions$before <- labelOptionsBefore
+    options$labelOptions$after <- labelOptionsAfter
+    options$labelOptions$onHover <- labelOptionsOnHover
+  }
+
+  options
 }
